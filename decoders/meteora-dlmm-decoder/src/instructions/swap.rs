b54c696200443a841d1eea3e878243e1bd35c180
kailanyue <yuekailan@163.com>
2025-02-06 17:45:21 +0800

    feature: meteora swap.rs add arrange_accounts function

--- decoders/meteora-dlmm-decoder/src/instructions/swap.rs
+++ decoders/meteora-dlmm-decoder/src/instructions/swap.rs
@@ -17,3 +17,63 @@ pub struct Swap {
     pub fee_bps: u128,
     pub host_fee: u64,
 }
+
+pub struct SwapInstructionAccounts {
+    pub lb_pair: solana_sdk::pubkey::Pubkey,
+    pub bin_array_bitmap_extension: solana_sdk::pubkey::Pubkey,
+    pub reserve_x: solana_sdk::pubkey::Pubkey,
+    pub reserve_y: solana_sdk::pubkey::Pubkey,
+    pub user_token_in: solana_sdk::pubkey::Pubkey,
+    pub user_token_out: solana_sdk::pubkey::Pubkey,
+    pub token_x_mint: solana_sdk::pubkey::Pubkey,
+    pub token_y_mint: solana_sdk::pubkey::Pubkey,
+    pub oracle: solana_sdk::pubkey::Pubkey,
+    pub host_fee_in: solana_sdk::pubkey::Pubkey,
+    pub user: solana_sdk::pubkey::Pubkey,
+    pub token_x_program: solana_sdk::pubkey::Pubkey,
+    pub token_y_program: solana_sdk::pubkey::Pubkey,
+    pub event_authority: solana_sdk::pubkey::Pubkey,
+    pub program: solana_sdk::pubkey::Pubkey,
+}
+
+impl carbon_core::deserialize::ArrangeAccounts for Swap {
+    type ArrangedAccounts = SwapInstructionAccounts;
+
+    fn arrange_accounts(
+        accounts: Vec<solana_sdk::instruction::AccountMeta>,
+    ) -> Option<Self::ArrangedAccounts> {
+        let lb_pair = accounts.get(0)?;
+        let bin_array_bitmap_extension = accounts.get(1)?;
+        let reserve_x = accounts.get(2)?;
+        let reserve_y = accounts.get(3)?;
+        let user_token_in = accounts.get(4)?;
+        let user_token_out = accounts.get(5)?;
+        let token_x_mint = accounts.get(6)?;
+        let token_y_mint = accounts.get(7)?;
+        let oracle = accounts.get(8)?;
+        let host_fee_in = accounts.get(9)?;
+        let user = accounts.get(10)?;
+        let token_x_program = accounts.get(11)?;
+        let token_y_program = accounts.get(12)?;
+        let event_authority = accounts.get(13)?;
+        let program = accounts.get(14)?;
+
+        Some(SwapInstructionAccounts {
+            lb_pair: lb_pair.pubkey,
+            bin_array_bitmap_extension: bin_array_bitmap_extension.pubkey,
+            reserve_x: reserve_x.pubkey,
+            reserve_y: reserve_y.pubkey,
+            user_token_in: user_token_in.pubkey,
+            user_token_out: user_token_out.pubkey,
+            token_x_mint: token_x_mint.pubkey,
+            token_y_mint: token_y_mint.pubkey,
+            oracle: oracle.pubkey,
+            host_fee_in: host_fee_in.pubkey,
+            user: user.pubkey,
+            token_x_program: token_x_program.pubkey,
+            token_y_program: token_y_program.pubkey,
+            event_authority: event_authority.pubkey,
+            program: program.pubkey,
+        })
+    }
+}
